3642304ed8f902bf4f99dd6a89f1a15597c310cc
Cameron White <cameronwhite35288@gmail.com>
2024-06-21 19:42:41 -0400

    update

--- src/election-navbar.js
+++ src/election-navbar.js
@@ -7,16 +7,16 @@ class ElectionNavBar extends React.Component{
     render(){
         return (
             <div id='election-header'>
-                <Link to='/' className='election-logo'>
+                <Link to='/anctimes-elections' className='election-logo'>
                     <rect className="two"><b><span className="two-text">2</span></b></rect>
                     <rect className="zero"><b><span className="two-text">0</span></b></rect>
                     <rect className="two-1"><b><span className="two-text">2</span></b></rect>
                     <rect className="four"><b><span className="two-text">4</span></b></rect>
                 </Link>
-                <Link to="/anchorage" className='anchorage-head'>Anchorage</Link>
-                <Link to="/ridgeway" className='ridgeway-head'>Ridgeway</Link>
-                <Link to="/harrison" className='harrison-head'>Harrison</Link>
-                <Link to="/state-results" className='stateresults-head'>State Results <KeyboardArrowDownIcon className="downarrow" style={{ fontSize: 12}}></KeyboardArrowDownIcon></Link>
+                <Link to="/anctimes-elections/anchorage" className='anchorage-head'>Anchorage</Link>
+                <Link to="/anctimes-elections/ridgeway" className='ridgeway-head'>Ridgeway</Link>
+                <Link to="/anctimes-elections/harrison" className='harrison-head'>Harrison</Link>
+                <Link to="/anctimes-elections/state-results" className='stateresults-head'>State Results <KeyboardArrowDownIcon className="downarrow" style={{ fontSize: 12}}></KeyboardArrowDownIcon></Link>
             </div>
         );
     }
