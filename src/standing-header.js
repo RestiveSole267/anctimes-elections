0a66dbe88ac3cde217cccb46c6eb684c35e057ec
Cameron White <cameronwhite35288@gmail.com>
2024-06-23 03:51:16 -0400

    updating logo

--- src/standing-header.js
+++ src/standing-header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import RSPAN from "./R-SPAN.png"
 
 class StandingHeader extends React.Component{
     render(){
         return (
             <div id='standing-header'>
-                <a href="https://restivesole267.github.io/anctimes/" className='header'>TheAnchorageTimes</a>
+                <div className="sub-headers">
+                    <a href="https://ro-span.org/" className='header'><img src={RSPAN} width="87" height="20" className="R-SPAN" atl="R-SPAN"></img>/ TheAnchorageTimes</a>
+                </div>
             </div>
         );
     }
